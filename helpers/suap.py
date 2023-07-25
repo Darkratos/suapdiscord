004968f76e62c391485551ede8af33210427bdf3
Xand <alexandre_fernandescosta@hotmail.com>
2023-07-24 21:22:39 -0300

    object oriented structure and choice commands

---
+++ helpers/suap.py
@@ -0,0 +1,134 @@
+import json
+import pickle
+from bs4 import BeautifulSoup as bs
+from requests import Session
+
+
+class Subject( ):
+    def __init__( self, name: str = None ) -> None:
+        self.name: str = name
+        self.total_grade: float = 0
+        self.grades: list[tuple[str, str]] = [ ]
+        self.absence: int = 0
+
+class Suap( ):
+    def __init__( self ) -> None:
+        self.session: Session = Session( )
+        self.headers: dict[str, str] = {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Referer': 'https://suap.ifsuldeminas.edu.br/accounts/login/'
+        }
+        
+        self.creds: dict[str, str] = self.load_creds( )
+        self.soup: bs = self.login( )
+        
+        self.current_period: str = self.load_period( )
+    
+    def get_soup_instance( self, url, headers= None ) -> bs:
+        page = self.session.get( url= url, headers= headers )
+        return bs( page.text, 'html.parser' )
+    
+    def load_creds( self ) -> dict:
+        creds = None
+        
+        try:
+            with open( "configs/suap_creds.json", "r" ) as file:
+                creds = json.load(file)
+        except: 
+            print( "[!] Erro em carregar notas.json" )
+            exit( )
+        
+        return creds
+        
+    def login( self ) -> bs:
+        parser = self.get_soup_instance( url= 'https://suap.ifsuldeminas.edu.br/accounts/login/', headers= self.headers )
+        token = parser.find( 'input', { 'type': 'hidden' } )[ 'value' ]
+
+        post_data = {
+            'csrfmiddlewaretoken': token,
+            'username': self.creds[ 'user' ],
+            'password': self.creds[ 'pass' ],
+            'this_is_the_login_form': '1',
+            'next': '/',
+            'g-recaptcha-response': ''
+        }
+
+        self.session.post( url= 'https://suap.ifsuldeminas.edu.br/accounts/login/', headers= self.headers, data= post_data )
+        
+        return self.get_soup_instance( url= f'https://suap.ifsuldeminas.edu.br/edu/aluno/{ self.creds[ "user" ] }/?tab=boletim', headers= self.headers )  
+
+    def load_period( self ) -> str:
+        period_selector: bs = self.soup.find( "select", { "id": "ano_periodo" } )
+        return period_selector.find_next( "option" ).get_text( ).replace( "/", "-" )
+        
+    def get_subjects( self ) -> list[Subject]:
+        if self.soup.find( "p", { "class" : "msg alert" } ).get_text( ):
+            return []
+        
+        subject_rows_tag: list[bs] = self.soup.find( "table" ).find( "tbody" ).find_all( 'tr' )
+        
+        subjects_list = []
+        
+        for tag in subject_rows_tag:
+            column_tag: list[bs] = tag.find_all( "td" )
+            
+            subject = Subject( )
+            subject.name = column_tag[ 1 ].get_text( ).split( "-" )[1]
+            subject.absence = int( column_tag[4].get_text( ) )
+            subject.total_grade = float( column_tag[11].get_text( ) )
+            
+            popup_tag = column_tag[ 12 ].find_next( "a" )
+            soup = self.get_soup_instance( f"https://suap.ifsuldeminas.edu.br{ popup_tag[ 'href' ] }?_popup=1", self.headers )
+                        
+            grade_tags: list[bs] = soup.find( "table" ).find( "tbody" ).find_all( 'tr' )
+            for tag in grade_tags:
+                column_tags: list[bs] = tag.find_all( 'td' )[ 0 : 6 ]
+                sigla, _, descricao, _, valor, nota_obtida = [ tag.get_text() for tag in column_tags ]
+
+                subject.grades.append( ( ( descricao if descricao != '-' else sigla ), f'{ nota_obtida } / { valor }' ))
+                
+            subjects_list.append( subject )
+        
+        return subjects_list
+            
+    def get_json_subjects( self ) -> dict:
+        try:
+            with open( f"periods/{self.current_period}.json", "rb" ) as file:
+                subjects = pickle.load(file)
+                
+                return subjects
+            
+        except FileNotFoundError:
+            print( f"[!] Criando {self.current_period}.json" )
+            empty_list = []
+            with open( f"{self.current_period}.json", "wb" ) as file:
+                pickle.dump( empty_list, file, protocol= pickle.HIGHEST_PROTOCOL )
+                
+            return empty_list
+                
+        except IOError:
+            print( f"[!] Erro em abrir {self.current_period}.json" )
+            exit( )
+            
+        except Exception as e:
+            print( f"[!] Exception: {e}" )
+            exit( )
+
+    def write_json_subjects( self, subjects: list[Subject] ):
+        try:
+            with open( f"periods/{self.current_period}.json", "wb" ) as file:
+                pickle.dump( subjects, file, protocol= pickle.HIGHEST_PROTOCOL )
+                
+        except IOError:
+            print( f"[!] Erro em abrir {self.current_period}.json" )
+            exit( )
+            
+        except Exception as e:
+            print( f"[!] Exception: {e}" )
+            exit( )
+
+    def get_subject( self, name: str ) -> Subject:
+        for subject in self.get_subjects( ):
+            if subject.name == name:
+                return subject
--- main.py
+++ main.py
@@ -1,107 +1,44 @@
 import discord
+from discord import app_commands
 from discord.ext import commands, tasks
-from requests import Session
-from bs4 import BeautifulSoup as bs
-import json
-import pickle
 from table2ascii import table2ascii as t2a, PresetStyle
-import sys
-import time
-
-class Suap( ):
-    def __init__( self ) -> None:
-        self.creds = None
-        self.session = Session( )
-        self.headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-            'Referer': 'https://suap.ifsuldeminas.edu.br/accounts/login/'
-        }
-        
-        self.load_creds( )
-    
-    def load_creds( self ):
-        try:
-            with open( "creds.json", "r" ) as file:
-                self.creds = json.load( file )   
-        except: 
-            print( "[!] Erro em carregar notas.json" )
-        
-    def login( self ):
-        parser = self.get_soup_instance( url= 'https://suap.ifsuldeminas.edu.br/accounts/login/', headers= self.headers )
-        token = parser.find( 'input', { 'type': 'hidden' } )[ 'value' ]
-
-        post_data = {
-            'csrfmiddlewaretoken': token,
-            'username': self.creds[ 'user' ],
-            'password': self.creds[ 'pass' ],
-            'this_is_the_login_form': '1',
-            'next': '/',
-            'g-recaptcha-response': ''
-        }
-
-        self.session.post( url= 'https://suap.ifsuldeminas.edu.br/accounts/login/', headers= self.headers, data= post_data )
-        return self.get_soup_instance( url= f'https://suap.ifsuldeminas.edu.br/edu/aluno/{ self.creds[ "user" ] }/?tab=boletim', headers= self.headers )
-        
-    def get_soup_instance( self, url, headers= None ):
-        page = self.session.get( url= url, headers= headers )
-        return bs( page.text, 'html.parser' )
-
-def remove_unicode( str ):
-        encoded_string = str.encode( 'ascii', 'ignore' )
-        return encoded_string.decode( )  
-
+from helpers.suap import Suap, Subject
+from helpers.utils import get_discord_config
+  
 def main( ):
+    token, channels_names, server_id = get_discord_config( )
     suap = Suap( )
-    bot = commands.Bot( command_prefix= "1", intents= discord.Intents.default( ) )
+    
+    intents = discord.Intents.default()
+    intents.message_content = True
+    bot = commands.Bot( command_prefix= "1", intents= intents )
 
-    @bot.tree.command( name="full", description= "Mostra todas notas e faltas", guild= discord.Object( id= 740695714316943442 ) ) 
+    @bot.tree.command( name="full", description= "Mostra todas notas e faltas" ) 
     async def full( interaction: discord.Interaction ):
-        soup = suap.login( )
-        materia_rows = soup.select( ".borda > tbody:nth-child(2)" )[ 0 ].find_all( 'tr' )
-
         body = []
-        idx = 0
-        for tag in materia_rows:
-            splits = tag.find_all( 'td' )
-            materia = splits[ 1 ].text.split( ' - ' )[ 1 ]
-            faltas = splits[ 8 ].text
-            nota = splits[ 7 ].text
+        
+        for subject in suap.get_subjects( ):
+            body.append( [subject.name, subject.absence, subject.total_grade] )
             
-            body.append( [ idx, materia, faltas, nota ] )
-            idx += 1
-
         output = t2a(
-            header = [ "Index", "Matéria", "Faltas", "Nota" ],
+            header = [ "Matéria", "Faltas", "Nota" ],
             body = body,
             style= PresetStyle.thin_box
         )
 
         await interaction.response.send_message( content = f"```\n{ output }\n```" )
 
-    @bot.tree.command( name="detalhes", description= "Detalha uma matéria", guild= discord.Object( id= 740695714316943442 ) ) 
-    async def detalhes( interaction: discord.Interaction, index: int ):
-        soup = suap.login( )
-        materia_popups_tags = soup.find_all( "a", { 'class': 'btn popup' } ) 
+    @bot.tree.command( name= "detalhes", description= "Detalha uma matéria" ) 
+    @app_commands.choices( choices= [ discord.app_commands.Choice( name= subject.name, value= subject.name ) for subject in suap.get_subjects( ) ] )
+    async def detalhes( interaction: discord.Interaction, choices: app_commands.Choice[str] ):
+        print(choices.name)
 
-        if index > len( materia_popups_tags ):
-            await interaction.response.send_message( content = "Index fora do range da lista de matérias..." )
-            return
-
-        tag = materia_popups_tags[ index ]
-
-        soup = suap.get_soup_instance( f"https://suap.ifsuldeminas.edu.br{ tag[ 'href' ] }?_popup=1", suap.headers )
-        materia = remove_unicode( soup.select( '.title-container > h2:nth-child(1)' )[ 0 ].text[ 7 : ] )
-        
+        subject = suap.get_subject( choices.name )
         body = []
-        nota_tags = soup.select( 'html body.theme-luna.popup_ div.holder main#content div.box div table.borda tbody tr' )
         
-        for tag in nota_tags:
-            sigla, _, descricao, _, valor, nota_obtida = [ tag.text for tag in tag.find_all( 'td' )[ 0 : 6 ] ]
-
-            if ( nota_obtida != '-' ):
-                body.append( [ descricao if descricao != '-' else sigla, f'{ nota_obtida } / { valor }' ] )
-
+        for name, grade in subject.grades:
+            body.append( [ name, grade ] )
+        
         output = t2a(
             header = [ "Atividade", "Nota" ],
             body = body,
@@ -112,65 +49,66 @@ def main( ):
 
     @tasks.loop( minutes = 10 )
     async def check( ):
-        soup = suap.login( )
-        materia_popups_tags = soup.find_all( "a", { 'class': 'btn popup' } ) 
-
-        dict_materias = { }
-        for tag in materia_popups_tags:
-            soup = suap.get_soup_instance( f"https://suap.ifsuldeminas.edu.br{ tag[ 'href' ] }?_popup=1", suap.headers )
-            materia = remove_unicode( soup.select( '.title-container > h2:nth-child(1)' )[ 0 ].text[ 7 : ] )
-            
-            dict_materias[ materia ] = { }
-
-            nota_tags = soup.select( 'html body.theme-luna.popup_ div.holder main#content div.box div table.borda tbody tr' )
-            
-            for tag in nota_tags:
-                sigla, _, descricao, _, valor, nota_obtida = [ tag.text for tag in tag.find_all( 'td' )[ 0 : 6 ] ]
-
-                if ( nota_obtida != '-' ):
-                    dict_materias[ materia ][ descricao if descricao != '-' else sigla ] = f'{ nota_obtida } / { valor }'
-
-        old_json = { }
+        updated_subjects = suap.get_subjects( )
+        old_subjects = suap.get_json_subjects( )
+        subjects_with_new_grades: list[Subject] = [ ]
+        subjects_with_new_absence: list[Subject] = [ ]
         
-        try:
-            with open( "notas.json", "rb" ) as file:
-                old_json = pickle.load( file )
+        for old_subject, updated_subject in zip( old_subjects, updated_subjects ):
+            old_subject: Subject
+            updated_subject: Subject
+            
+            new_grades = [ updated_grade for old_grade, updated_grade in zip(old_subject.grades, updated_subject.grades) if updated_grade[1] != old_grade[1] ]            
+            
+            if len( new_grades ):
+                subjects_with_new_grades.append( updated_subject )
                 
-        except FileNotFoundError:
-            print( "[!] Criando notas.json" )
-            with open( "notas.json", "wb" ) as file:
-                pickle.dump( dict_materias, file, protocol= pickle.HIGHEST_PROTOCOL )
-                
-        except IOError:
-            print( "[!] Erro em abrir notas.json" )
+            if old_subject.absence != updated_subject.absence:
+                subjects_with_new_absence.append( updated_subject )
+        
+        if len( subjects_with_new_grades ):
+            print( "[!] Novas notas" )
+            suap.write_json_subjects( updated_subjects )
             
-        except Exception as e:
-            print( f"[!] Exception: {e}" )
-            
-        with open( "notas.json", "wb" ) as file:
-            novo = { materia: dict_materias[ materia ] for materia in dict_materias.keys( ) if materia in old_json.keys( ) and dict_materias[ materia ] != old_json[ materia ] }
-            pickle.dump( dict_materias, file, protocol= pickle.HIGHEST_PROTOCOL )
-            
-        if len( novo ):
             for server in bot.guilds:
-                channel = discord.utils.get( server.channels, name = "notas" )
+                channel = discord.utils.get( server.channels, name= channels_names[ "grades" ] )
+                
+                body = [ ]
+                
+                for subject in subjects_with_new_grades:
+                    for name, grade in subject.grades:
+                        body.append( [ name, grade ] )
+                
+                output = t2a(
+                    header = [ "Atividade", "Nota" ],
+                    body = body,
+                    style= PresetStyle.thin_box
+                )
 
-                text = ''
-                for materia in novo:
-                    title = materia
-                    
-                    text = "\n".join( [ f"{key}" for key in dict_materias[ materia ] ] )
+                await channel.send( "<@Suap> ", embed= output )
 
-                    embed = discord.Embed( title= title, description= text, color= discord.Color.blue( ), url= 'https://suap.ifsuldeminas.edu.br/accounts/login' )
-                    await channel.send( "<@here> ", embed= embed )
+        if len( subjects_with_new_absence ):
+            print("[!] Novas faltas")
+            suap.write_json_subjects( updated_subjects )
+            
+            for server in bot.guilds:
+                channel = discord.utils.get( server.channels, name= channels_names[ "absences" ] )
+                
+                output = t2a(
+                    header = [ "Matéria", "Faltas" ],
+                    body = [ [ subject.name, subject.absence ] for subject in subjects_with_new_grades ],
+                    style= PresetStyle.thin_box
+                )
+
+                await channel.send( "<@here> ", embed= output )
 
     @bot.event
     async def on_ready( ):
-        await bot.tree.sync( guild= discord.Object( id= 740695714316943442 ) )
+        await bot.tree.sync( guild=discord.Object( id= server_id) )
 
         check.start( )
 
-    bot.run( suap.creds[ "token" ] )
+    bot.run( token )
     
 if __name__ == "__main__":
     main( )
